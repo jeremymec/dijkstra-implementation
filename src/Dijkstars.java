19e345f8da59c83ae008314c3b9a70862bf84de7
jezza97 <jeremymechen1997@gmail.com>
2017-04-13 03:35:29 +1200

    Dij class fleshed out

--- src/Dijkstars.java
+++ src/Dijkstars.java
@@ -1,9 +1,27 @@
-
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dijkstars {
     Graph g;
+    HashMap<Node, Integer> unvisited;
 
     public Dijkstars(Graph g){
         this.g = g;
+
+        for(Node n : g.nodes){
+            unvisited.put(n, Integer.MAX_VALUE);
+        }
+    }
+
+    public Node[] find(Node start, Node end){
+
+
+
+
+
+
+
+
+        return null;
     }
 }
