3abd985535a752df01241176c75189d58fd9fff2
jezza97 <jeremymechen1997@gmail.com>
2017-04-13 03:00:42 +1200

    Fleshed out Node file

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
 
 public class Node {
+
+    private ArrayList<Edge> edges;
+
+    public Node(){
+        this.edges = new ArrayList<>();
+    }
+
+    public void addEdge(Edge e){
+        this.edges.add(e);
+    }
+
+    public ArrayList<Edge> getEdges(){
+        return this.edges;
+    }
+
 }
