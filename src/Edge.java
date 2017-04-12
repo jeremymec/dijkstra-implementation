23ef4476d99ebdb74b1ca8be0e76cb69a742faa1
jezza97 <jeremymechen1997@gmail.com>
2017-04-13 02:54:44 +1200

    Edge file fleshed out

--- src/Edge.java
+++ src/Edge.java
@@ -1,3 +1,25 @@
 
+
 public class Edge {
+
+    private Node first;
+    private Node second;
+
+    private int weight;
+
+    public Edge(Node first, Node second, int weight){
+        this.first = first;
+        this.second = second;
+        this.weight = weight;
+    }
+
+    public Node[] getNodes(){
+        Node[] nodes = {this.first, this.second};
+        return nodes;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+
 }
